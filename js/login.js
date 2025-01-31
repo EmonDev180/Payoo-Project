1289ee1605426b9ce8b8095eab20d7acd1fe5ce9
Emon Dev180 <shuvroemon.cse@gmail.com>
2025-01-31 20:55:03 +0600

    main section commit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-login').addEventListener('click',function(e){
+
+    e.preventDefault()
+
+    const phoneNumber = document.getElementById('phone-number');
+
+    console.log(phoneNumber.value)
+})
